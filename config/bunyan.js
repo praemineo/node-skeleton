2bf243042a5749735bc019fd421f737d9b38ba4c
errhunter <imgg.gauravgandhi@gmail.com>
2017-03-23 11:29:28 +0530

    Logsadded

--- config/bunyan.js
+++ config/bunyan.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
         stream: formatOut,
       }, {
         level: 'error',
-        path: `${appRootDir}/log_store/error.development.log`,
+        path: `${appRootDir}/logs/error.development.log`,
       }],
     });
   };
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'development') {
       serializers: bunyan.stdSerializers,
       streams: [{
         level: 'info',
-        path: `${appRootDir}/log_store/info.production.log`
+        path: `${appRootDir}/logs/info.production.log`
       }, {
         level: 'error',
         stream: new bunyanSlack({
