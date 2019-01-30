fc8eceeaebda6b6b91e8aa8c463327b43e8eeb73
Elvis Fernandes <elvis.f@workstreets.com>
2019-01-30 19:10:44 +0530

    implement try-catch block

--- initServer.js
+++ initServer.js
@@ -1,61 +1,67 @@
 const { readFileSync } = require('fs');
 
 const initServer = async (config, app) => {
-  const serverObject = {};
+  const logger = config.logger.createLogger('initServer');
 
-  if (config.app.server.protocol == 'https') {
-    //https server
-    const https = require('https');
-    
-    // loading SSL certificates
-    serverObject.server = https.createServer({
-      key: readFileSync('config/ssl/private.key'),
-      cert: readFileSync('config/ssl/certificate.crt'),
-      ca: readFileSync('config/ssl/ca_bundle.crt')
-    }, app);
+  try {
+    const serverObject = {};
 
-  } else {
-    //http server init
-    const http = require('http');
-    serverObject.server = http.createServer(app);
-  }
+    if (config.app.server.protocol == 'https') {
+      //https server
+      const https = require('https');
 
-  if (config.app.server.socket) {
-    //init socket io
-    const socket = require('socket.io');
+      // loading SSL certificates
+      serverObject.server = https.createServer({
+        key: readFileSync('config/ssl/private.key'),
+        cert: readFileSync('config/ssl/certificate.crt'),
+        ca: readFileSync('config/ssl/ca_bundle.crt')
+      }, app);
 
-    if (config.app.server.socket.port) {
-      //if port is present then init socket on that port
-      serverObject.io = socket(config.app.server.socket.port);
     } else {
-      // else use the provieded server to init the socket
-      serverObject.io = socket(serverObject.server);
+      //http server init
+      const http = require('http');
+      serverObject.server = http.createServer(app);
     }
-  }
 
-  if (config.app.db) {
-    //mongo db initiaization.
-    const mongoose = require('mongoose');
-    mongoose.Promise = Promise;
+    if (config.app.server.socket) {
+      //init socket io
+      const socket = require('socket.io');
 
-    //if authentication available, then use the following.
-    const auth = config.app.db.username && config.app.db.password ? {
-      user: config.app.db.username,
-      pass: config.app.db.password,
-      auth: {
-        authDB: 'admin'
+      if (config.app.server.socket.port) {
+        //if port is present then init socket on that port
+        serverObject.io = socket(config.app.server.socket.port);
+      } else {
+        // else use the provieded server to init the socket
+        serverObject.io = socket(serverObject.server);
       }
-    } : {};
+    }
 
-    //connecting to mongoose.
-    await mongoose.connect(`mongodb://${config.app.db.host}/${config.app.db.database}`, {
-      ...auth,
-      useNewUrlParser: true
-    });
+    if (config.app.db) {
+      //mongo db initiaization.
+      const mongoose = require('mongoose');
+      mongoose.Promise = Promise;
+
+      //if authentication available, then use the following.
+      const auth = config.app.db.username && config.app.db.password ? {
+        user: config.app.db.username,
+        pass: config.app.db.password,
+        auth: {
+          authDB: 'admin'
+        }
+      } : {};
+
+      //connecting to mongoose.
+      await mongoose.connect(`mongodb://${config.app.db.host}/${config.app.db.database}`, {
+        ...auth,
+        useNewUrlParser: true
+      });
+    }
+
+    return serverObject;
+
+  } catch (error) {
+    logger.error(error);
   }
-
-  return serverObject;
 };
 
 module.exports = initServer;
-
