9d2eb0970500962e2211709ff325f3c05f029d33
Elvis Fernandes <elvis.f@workstreets.com>
2019-01-30 19:10:44 +0530

    implement try-catch block

--- init.js
+++ init.js
@@ -10,60 +10,62 @@ const responseTime = require('response-time');
 const initServer = require(path.join(__dirname, 'initServer'));
 
 module.exports.init = async function init() {
+  try {
+    const app = express();
 
+    //the custom userObject, that is shared over the application via the require cache.
+    require.cache.userObject = {
+      app
+    };
+
+    //the base __dirname
+    require.cache.userObject.appPath = __dirname;
+
+    app.use(compression());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+      extended: true,
+    }));
+    app.use(cookieParser());
+    app.use(helmet());
+    app.use(cors());
+    app.use(responseTime());
+
+    //initializing config and getting the config object
+    const config = require(path.join(__dirname, '/config/index.js'));
+
+    //initializing errorObject
+    const errorObject = require(path.join(__dirname, '/config/error.js'));
+    require.cache.userObject.errorObject = errorObject;
+
+    //loading the base application router.
+    const appRouter = require(path.join(__dirname, '/routes.js'));
+
+    //initialzing the logger with the module name.
+    const logger = config.logger.createLogger('init');
+
+    // Router mounting
+    app.use('/', appRouter);
+
+    //initizing the server, with config and app.
+    const { io, server } = await initServer(config, app) || {};
+
+    //passing the socketObject to the userObject
+    require.cache.userObject.io = io;
+
+    //server object
+    server.listen(config.app.server.port)
+      .on('error', error => logger.error(error))
+      .on('listening', () => logger.info(`Express listening on ${config.app.server.port}`));
+
+    //catching uncaught excpetions and terminating the application.
+    process.on('uncaughtException', (err) => {
+      console.error('whoops! there was an error', errorObject.getError('UNCAUGHT_EXCEPTION', err));
+      process.exit(0);
+    });
+
+  } catch (error) {
+    console.error('Error in initializing', error);
+  }
   //init express
-  const app = express();
-
-  //the custom userObject, that is shared over the application via the require cache.
-  require.cache.userObject = {
-    app 
-  };
-
-  //the base __dirname
-  require.cache.userObject.appPath = __dirname; 
-
-  app.use(compression());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true,
-  }));
-  app.use(cookieParser());
-  app.use(helmet());
-  app.use(cors());
-  app.use(responseTime());
-
-  //initializing config and getting the config object
-  const config = require(path.join(__dirname, '/config/index.js'));
-
-  //initializing errorObject
-  const errorObject = require(path.join(__dirname, '/config/error.js'));
-  require.cache.userObject.errorObject = errorObject;
-
-  //loading the base application router.
-  const appRouter = require(path.join(__dirname, '/routes.js'));
-
-  //initialzing the router with the module name.
-  const logger = config.logger.createLogger('init');
-
-  // Router mounting
-  app.use('/', appRouter);
-
-  //initizing the server, with config and app.
-  const { io, server } = await initServer(config, app);
-
-  //passing the socketObject to the userObject
-  require.cache.userObject.io = io;
-
-  //server object
-  server.listen(config.app.server.port)
-    .on('error', error => logger.error(error))
-    .on('listening', () => logger.info(`Express listening on ${config.app.server.port}`));
-
-  //catching uncaught excpetions and terminating the application.
-  process.on('uncaughtException', (err) => {
-    console.error('whoops! there was an error', errorObject.getError('UNCAUGHT_EXCEPTION', err));
-    process.exit(0);
-  });
-
 };
-
--- user_modules/sample/crud.js
+++ user_modules/sample/crud.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const config = require(path.resolve(require.cache.userObject.appPath, 'config/index.js'));
-// eslint-disable-next-line no-unused-vars
 const logger = config.logger.createLogger('clone/crud');
 
 const greetUser = (req, res, next) => {
-  res.locals.greetMsg = 'Hello User, Welcome to Node Skeleton';
-  next();
+  try {
+    res.locals.greetMsg = 'Hello User, Welcome to Node Skeleton';
+    next();
+  } catch (error) {
+    logger.error(error);
+    next(error); 
+  }
 };
 
 const squareNumber = (req, res, next) => {
-  res.locals.result = `Square of ${req.params.number} is ${parseFloat(req.params.number) * parseFloat(req.params.number)}`;
-  next();
+  try {
+    res.locals.result = `Square of ${req.params.number} is ${parseFloat(req.params.number) * parseFloat(req.params.number)}`;  
+    next();
+  } catch (error) {
+    logger.error(error);
+    next(error); 
+  }
+  
 };
 
 module.exports = {greetUser, squareNumber};
